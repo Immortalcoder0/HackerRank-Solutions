49e3612a418b247a0fb5040805005b839a8d51fe
Subhankar Das <being.subh90@gmail.com>
2024-12-17 18:32:36 +0530

    Update Printing Tokens.c

--- C language/Printing Tokens.c
+++ C language/Printing Tokens.c
@@ -1,16 +1,13 @@
 #include <stdio.h> 
-int main() { 
-    int n; 
-
-    scanf("%d", &n); 
-    int arr[n]; 
+int main()  
+{ 
+    char str[1001]; 
   
-    for (int i = 0; i < n; i++) { 
-        scanf("%d", &arr[i]); 
+    scanf("%[^\n]s", str); 
+   
+    for (int i = 0; str[i] != '\0'; i++) 
+    { 
+        if (str[i] == ' ') printf("\n"); 
+        else printf("%c", str[i]); 
     } 
- 
-    for (int i = n - 1; i >= 0; i--) { 
-        printf("%d ", arr[i]); 
-    } 
-    return 0; 
-}
+} 
